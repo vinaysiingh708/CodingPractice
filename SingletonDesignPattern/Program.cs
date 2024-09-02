3de6eee50ea867e961eaaa7fc1f3a92c8a78f7c0
vinay singh <vinaysingh708@yahoo.com>
2024-09-02 10:33:14 +0530

    Singleton Design Pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        // Private constructor to prevent instantiation from outside
+        private Singleton()
+        {
+        }
+
+        // Public property to provide global access to the instance
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        // Example method to demonstrate the Singleton usage
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton instance method called.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Both instances are the same.");
+            }
+
+            singleton1.DoSomething();
+        }
+    }
+}
